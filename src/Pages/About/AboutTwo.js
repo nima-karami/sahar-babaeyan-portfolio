bf0e3dc056a2c71eba6d10247739b1ff0d755255
Nima Karami <karami.nima@live.com>
2022-08-09 12:05:17 -0400

    updating about me section

--- src/Pages/About/AboutTwo.js
+++ src/Pages/About/AboutTwo.js
@@ -28,15 +28,27 @@ const AboutTwo = () => {
               <div className="col-span-12 space-y-2.5">
                 <div className="lg:mr-16">
                   <p className="text-gray-lite  dark:text-color-910 leading-7">
-                    I'm Creative Director and UI/UX Designer from Sydney,
-                    Australia, working in web development and print media. I
-                    enjoy turning complex problems into simple, beautiful and
-                    intuitive designs.
+                    A creative, passionate and innovative social media fanatic
+                    with 9+ years of professional experience with driving brands
+                    forward and achieving substantial growth in the digital space.
                   </p>
                   <p className="text-gray-lite leading-7 mt-2.5 dark:text-color-910">
-                    My aim is to bring across your message and identity in the
-                    most creative way. I created web design for many famous
-                    brand companies.
+                    With a personal first hand look at the influencer and social media world,
+                     I am well versed in the strategies and inner workings of how to successfully
+                     achieve desired results for any brand or client that comes my way.
+                  </p>
+                  <p className="text-gray-lite leading-7 mt-2.5 dark:text-color-910">
+                    Armed with a constantly updating knowledge bank on all things social, 
+                    I am equipped with what it takes to leverage brand awareness on a plethora of platforms. 
+                    Achieving success in the digital world requires the relentless drive to stay updated with 
+                    relevant content, all while engaging and creating stimulating yet genuine experiences in 
+                    order to build a loyal relationship with the audience.
+                  </p>
+                  <p className="text-gray-lite leading-7 mt-2.5 dark:text-color-910">
+                    In my role, I thrive on creative problem solving and come up with 
+                    effective and innovative ideas for those I work with. Make sure to review my 
+                    Social Media Portfolio for examples of my work, and to learn about clients 
+                    I’ve worked with in the past. I look forward to helping your business grow.
                   </p>
                 </div>
                 <div></div>
--- src/Share/Footer.js
+++ src/Share/Footer.js
@@ -13,14 +13,14 @@ const Footer = ({ bg, condition }) => {
       className="overflow-hidden rounded-b-2xl"
     >
       <p className="text-center py-6 text-gray-lite  dark:text-color-910 ">
-        © 2022 All Rights Reserved by{" "}
+        © {new Date().getFullYear()} All Rights Reserved by{" "}
         <a
           className="hover:text-[#FA5252] duration-300 transition"
-          href="https://themeforest.net/user/ib-themes"
+          href="sahar-babaeyan.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          ib-themes
+          Sahar Babaeyan
         </a>
         .
       </p>
