1a23b0a1d83e37d25cb3dc0ac193f02ab54204e3
Nima Karami <karami.nima@live.com>
2022-08-14 16:21:50 -0400

    adding CV

--- src/Share/HomeCard.js
+++ src/Share/HomeCard.js
@@ -82,7 +82,7 @@ const HomeCard = () => {
           </div>
           <button className="flex items-center mx-auto bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l from-[#DD2476]  to-[#fa5252ef] px-8 py-3 text-lg text-white rounded-[35px] mt-6">
             <img src={downloadIcon} alt="icon" className="mr-2" />
-            Download CV
+            <a href="src/assets/documents/Sahar_Babaeyan_Resume.pdf" download="Sahar_Babaeyan_Resume.pdf">Download CV</a>
           </button>
         </div>
       </div>
